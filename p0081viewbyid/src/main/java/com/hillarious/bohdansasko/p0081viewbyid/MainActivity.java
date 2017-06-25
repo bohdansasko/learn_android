3961272882de72be5598f5cc7e9f0c1dcfb6df1d
TQOS <vip.bod0410@gmail.com>
2017-06-25 14:35:00 +0300

    work with elements on activity from code

---
+++ p0081viewbyid/src/main/java/com/hillarious/bohdansasko/p0081viewbyid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hillarious.bohdansasko.p0081viewbyid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
