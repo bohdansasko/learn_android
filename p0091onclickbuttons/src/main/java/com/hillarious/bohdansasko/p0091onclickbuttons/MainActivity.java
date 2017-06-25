ab1f504ffaf789afa07139355a0544b47a260c6f
TQOS <vip.bod0410@gmail.com>
2017-06-25 22:51:20 +0300

    P0102_ActivityListener - use activity as click listener

--- p0091onclickbuttons/src/main/java/com/hillarious/bohdansasko/p0091onclickbuttons/MainActivity.java
+++ p0091onclickbuttons/src/main/java/com/hillarious/bohdansasko/p0091onclickbuttons/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     TextView m_tvOut;
     Button m_btnOk;
@@ -23,25 +23,23 @@ public class MainActivity extends AppCompatActivity {
         m_btnOk = (Button)findViewById(R.id.btnOk);
         m_btnCancel = (Button)findViewById(R.id.btnCancel);
 
+        m_btnOk.setOnClickListener(this);
+        m_btnCancel.setOnClickListener(this);
 
-        OnClickListener btnListener = new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.btnOk:
-                        m_tvOut.setText("Clicked button ok");
-                        break;
-                    case R.id.btnCancel:
-                        m_tvOut.setText("Clicked button cancel");
-                        break;
-                    default:
-                        m_tvOut.setText("Clicked button doesn't known");
-                }
-            }
-        };
+    }
 
-        m_btnOk.setOnClickListener(btnListener);
-        m_btnCancel.setOnClickListener(btnListener);
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnOk:
+                m_tvOut.setText("Clicked button ok");
+                break;
+            case R.id.btnCancel:
+                m_tvOut.setText("Clicked button cancel");
+                break;
+            default:
+                m_tvOut.setText("Clicked button doesn't known");
+        }
     }
 }
