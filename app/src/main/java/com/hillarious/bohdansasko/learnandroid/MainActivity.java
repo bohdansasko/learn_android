423d10bf2fe90b7e06c79f3c6683e0ccb6525173
TQOS <vip.bod0410@gmail.com>
2017-06-25 23:27:44 +0300

    P0111_ResValues - create and use resource file(activity_main.xml, source code)

--- app/src/main/java/com/hillarious/bohdansasko/learnandroid/MainActivity.java
+++ app/src/main/java/com/hillarious/bohdansasko/learnandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.hillarious.bohdansasko.learnandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        tvBottom.setText(R.string.tvBottomText);
+
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+        btnBottom.setText(R.string.btnBottomText);
     }
 }
