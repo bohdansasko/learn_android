d887653d440cd6aee5d0779f95c1e27639770f2d
TQOS <vip.bod0410@gmail.com>
2017-06-25 21:50:28 +0300

    find element by id and work with his properties

--- p0081viewbyid/src/main/java/com/hillarious/bohdansasko/p0081viewbyid/MainActivity.java
+++ p0081viewbyid/src/main/java/com/hillarious/bohdansasko/p0081viewbyid/MainActivity.java
@@ -2,6 +2,7 @@ package com.hillarious.bohdansasko.p0081viewbyid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOk = (Button)findViewById(R.id.btnOk);
+        btnOk.setText("Clicked!");
+        btnOk.setEnabled(false);
     }
 }
